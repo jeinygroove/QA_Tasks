434411e5e6d36d3b31578bc08749a2cd34e04d03
Ольга Шиманская <ol_shiman@mail.ru>
2021-04-09 00:55:36 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const dev = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-    server: dev ? 'http://localhost:3000' : 'https://cdn.statically.io/gh/jeinygroove/jeinygroove.github.io/QA_Tasks/gh-pagesт'
+    server: dev ? '' : 'https://cdn.statically.io/gh/jeinygroove/jeinygroove.github.io/QA_Tasks/gh-pages/'
 }
