9d4be2fa497010e21b67f0fa0467bbd960e09ead
Ольга Шиманская <ol_shiman@mail.ru>
2021-04-09 16:15:59 +0300

    add heroku url

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ import * as events from "events";
 
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://jeinygroove.github.io/QA_Tasks'
+export const server = dev ? 'http://localhost:3000' : 'https://qa-course-task1-books.herokuapp.com/'
 
 
 events.EventEmitter.prototype._maxListeners = 100;
