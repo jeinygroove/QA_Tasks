2d47f374e6884269047dbe2ab85fef4d4710e41c
Ольга Шиманская <ol_shiman@mail.ru>
2021-04-09 13:52:52 +0300

    fix4

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ import * as events from "events";
 
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : ''
+export const server = dev ? 'http://localhost:3000' : process.env.DEV_ULR;
 
 
 events.EventEmitter.prototype._maxListeners = 100;
