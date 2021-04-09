cb7e907db485963b9d612fc92b0768cd1f580efa
Ольга Шиманская <ol_shiman@mail.ru>
2021-04-09 14:03:33 +0300

    fix5

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ import * as events from "events";
 
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : process.env.DEV_ULR;
+export const server = dev ? 'http://localhost:3000' : 'https://olga-shim-blogr-nextjs-prisma.vercel.app';
 
 
 events.EventEmitter.prototype._maxListeners = 100;
